d3660d760298b0abaf68e2ecd9dc8699fee9a42f
Samouil <samoyhls@gmail.com>
2025-03-21 14:55:52 +0200

    migra

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,14 +1,23 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
-import { LandingComponent } from './landing/landing.component';
+import { Routes } from '@angular/router';
+import { AppLayout } from '../app/layout/component/app.layout';
+import { Dashboard } from '../app/pages/dashboard/dashboard';
+import { Documentation } from '../app/pages/documentation/documentation';
+import { Landing } from '../app/pages/landing/landing';
+import { Notfound } from '../app/pages/notfound/notfound';
 
 export const appRoutes: Routes = [
-  { path: '', component: LandingComponent },
-  // Add other routes here
+    {
+        path: '',
+        component: Notfound,
+        children: [
+            { path: '', component: Notfound },
+            { path: 'uikit', loadChildren: () => import('../app/pages/uikit/uikit.routes') },
+            { path: 'documentation', component: Documentation },
+            { path: 'pages', loadChildren: () => import('../app/pages/pages.routes') }
+        ]
+    },
+    { path: 'landing', component: Notfound },
+    { path: 'notfound', component: Notfound },
+    { path: 'auth', loadChildren: () => import('../app/pages/auth/auth.routes') },
+    { path: '**', redirectTo: '/notfound' }
 ];
-
-@NgModule({
-  imports: [RouterModule.forRoot(appRoutes)],
-  exports: [RouterModule]
-})
-export class AppRoutingModule { }
